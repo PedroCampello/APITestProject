4131b0ab7219e9cca11c34283061c184a8c28191
User <User@DESKTOP-87QMS9E>
2022-06-05 13:00:58 -0300

    Add project files.

---
+++ APITestProject/Zippopotamus.cs
@@ -0,0 +1,16 @@
+namespace APITestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldAssertAddress()
+        {
+            Assert.Pass();
+        }
+    }
+}
