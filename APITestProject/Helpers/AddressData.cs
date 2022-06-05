779bab5dda9dca5b02c7700e52b2000acd3daf13
pcampello <phcampello49@gmail.com>
2022-06-05 16:17:36 -0300

    Refactor to use a helper object

---
+++ APITestProject/Helpers/AddressData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestProject.Helpers
+{
+    public class Address
+    {
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string CountryAbbreviation { get; set; }
+        public string PlaceName { get; set; }
+        public string Longitude { get; set; }
+        public string State { get; set; }
+        public string StateAbbreviation { get; set; }
+        public string Latitude { get; set; }
+
+        public Address(string postalCode, string country, string countryAbbreviation, string placeName, string longitude, string state, string stateAbbreviation, string latitude)
+        {
+            this.PostalCode = postalCode.ToString();
+            this.Country = country.ToString();
+            this.CountryAbbreviation = countryAbbreviation.ToString();
+            this.PlaceName = placeName.ToString();
+            this.Longitude = longitude.ToString();
+            this.State = state.ToString();
+            this.StateAbbreviation = stateAbbreviation.ToString();
+            this.Latitude = latitude.ToString();
+        }
+    }
+}
--- APITestProject/Tests/Zippopotamus.cs
+++ APITestProject/Tests/Zippopotamus.cs
@@ -1,4 +1,5 @@
 using APITestProject.APIClass;
+using APITestProject.Helpers;
 using Newtonsoft.Json.Linq;
 
 namespace APITestProject.Tests
@@ -16,9 +17,10 @@ namespace APITestProject.Tests
         [Test]
         public void ShouldAssertAddressInformation()
         {
-            string countryEntry = "us";
-            string postalEntry = "50120";
-            JToken token = zippoAPICaller.GetResponseByZip(countryEntry, postalEntry);
+            var address = new Address("50120", "United States", "US", "Haverhill", "-92.9829", "Iowa", "IA", "41.9337");
+            string callCountry = address.CountryAbbreviation.ToLower();
+            string callPostal = address.PostalCode;
+            JToken token = zippoAPICaller.GetResponseByZip(callCountry, callPostal);
 
             string? postalCode = token?.SelectToken("['post code']")?.ToString();
             string? country = token?.SelectToken("country")?.ToString();
@@ -32,15 +34,15 @@ namespace APITestProject.Tests
 
             Assert.Multiple(() =>
             {
-                Assert.That(postalCode, Is.EqualTo("50120"));
-                Assert.That(country, Is.EqualTo("United States"));
-                Assert.That(countryAbbrev, Is.EqualTo("us".ToUpper()));
+                Assert.That(postalCode, Is.EqualTo(address.PostalCode));
+                Assert.That(country, Is.EqualTo(address.Country));
+                Assert.That(countryAbbrev, Is.EqualTo(address.CountryAbbreviation));
 
-                Assert.That(placeName, Is.EqualTo("Haverhill"));
-                Assert.That(longitude, Is.EqualTo("-92.9829"));
-                Assert.That(state, Is.EqualTo("Iowa"));
-                Assert.That(stateAbbrev, Is.EqualTo("IA"));
-                Assert.That(latitude, Is.EqualTo("41.9337"));
+                Assert.That(placeName, Is.EqualTo(address.PlaceName));
+                Assert.That(longitude, Is.EqualTo(address.Longitude));
+                Assert.That(state, Is.EqualTo(address.State));
+                Assert.That(stateAbbrev, Is.EqualTo(address.StateAbbreviation));
+                Assert.That(latitude, Is.EqualTo(address.Latitude));
             });
         }
     }
